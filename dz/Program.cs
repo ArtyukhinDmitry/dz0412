793507627a4c1a79342b473e1d0edb79731871dc
AD <artyukhindmitry@yandex.ru>
2022-12-06 17:28:56 +0400

    dz0312

---
+++ dz/Program.cs
@@ -0,0 +1,51 @@
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+// Console.Write("Введите пятизначное число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// int number1 = number / 10000;
+// int number22 = number % 10000;
+// int number2 = number22 / 1000;
+// int number44 = number % 100;
+// int number4 = number44 / 10;
+// int number5 = number % 10;
+// if (number1 == number5 && number2 == number4)
+// {
+//     Console.WriteLine("Число является палиндромом");
+// }
+// else Console.WriteLine("Число НЕ палиндромом");
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+// Console.Write("Введите координату х1: ");
+// int x1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату y1: ");
+// int y1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату z1: ");
+// int z1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату x2: ");
+// int x2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату y2: ");
+// int y2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите координату z2: ");
+// int z2 = Convert.ToInt32(Console.ReadLine());
+// double d = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+// Console.WriteLine($"Расстояние между 2 точками: {x1},{y1},{z1} и {x2},{y2},{z2} = {Math.Round(d,2)}");
+
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+// Console.Write("Введите число N: ");
+// int finish = Convert.ToInt32(Console.ReadLine());
+// for (int start = 1; start <= finish;  start++)
+// {
+//     Console.Write(Math.Pow(start,3) + " ");
+// }
